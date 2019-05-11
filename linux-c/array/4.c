07d06bcaf5944a7a9e2eb593e56fd3060630a73c
rurui <rurui739759848@live.com>
2019-05-11 08:52:23 -0700

    test_sizeof

--- linux-c/array/4.c
+++ linux-c/array/4.c
@@ -1,1 +1,11 @@
 //实现数组的全排列
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int a[3]={1,2,3};
+    int b=sizeof(a);
+    int c=sizeof(a)/sizeof(a[0]);
+    printf("%d %d\n",b,c);
+    return 0;
+}

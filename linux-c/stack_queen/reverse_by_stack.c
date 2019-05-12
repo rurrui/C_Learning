9d2a5a21260a401a3c2ee44078d778fd14f1d3c1
rurui <rurui739759848@live.com>
2019-05-12 08:23:05 -0700

    stack

---
+++ linux-c/stack_queen/reverse_by_stack.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+char stack[512];
+int top=0;
+
+void push(char c){
+    stack[top++]=c;
+}
+
+char pop(void){
+    return stack[--top];
+}
+
+int empty(void){
+    return top==0;
+}
+
+int main(void){
+    push('a');
+    push('b');
+    push('c');
+
+    while(!empty()){
+        putchar(pop());
+    }
+    putchar("\n");
+    return 0;
+}

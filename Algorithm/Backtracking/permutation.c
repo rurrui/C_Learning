96c199ce42c18c4f39e476a40c59e33cdb2507fc
rurui <rurui739759848@live.com>
2019-05-20 23:55:30 -0700

    leetcode 46

--- Algorithm/Backtracking/permutation.c
+++ Algorithm/Backtracking/permutation.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #define N 5
 
-void swap(int n[],int left,int right){
-    
+void swap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void permutation(int list[],int index,int size){
+    int i,j;
+
+    if(index==size){
+        for(i=0;i<size;i++){
+            printf("%d",list[i]);
+        }
+        printf("\n");
+    }
+    else{
+       for(j=index;j<size;j++){
+        swap(&list[j],&list[index]);
+        permutation(list,index+1,size);
+        swap(&list[j],&list[index]);
+       } 
+    }
 }
 
 int main(void){
-
+    int a[N]={1,2,3,4,5};
+    permutation(a,0,N);
+    return 0;
 }

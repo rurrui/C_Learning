2e959dc1e4e1ff115fe8b76479dc0c586b13a02d
rurui <rurui739759848@live.com>
2019-06-20 08:07:35 -0700

    gdb init

---
+++ linux-c/gdb/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int add_range(int low,int heigh){
+    int i,sum=0;
+    for(i=low;i<=heigh;i++){
+        sum+=i;
+    }
+    return sum;
+}
+
+int main(void){
+    int result[100];
+    result[0]=add_range(1,10);
+    result[1]=add_range(1,100);
+    printf("result[0]=%d\nresult[1]=%d\n",result[0],result[1]);
+    return 0;
+}
